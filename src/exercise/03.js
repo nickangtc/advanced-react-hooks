f5d793018d5b21b821818d9260f73917bc8b2231
Nick Ang <nickangtc@gmail.com>
2022-07-20 22:50:37 +0200

    03 - refactor to useCount hook to add error msg if out of contextprovider

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -7,12 +7,12 @@ import * as React from 'react'
 const CountContext = React.createContext()
 
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -27,10 +27,19 @@ function CountProvider({...props}) {
   )
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within CountProvider')
+  }
+  return context
+}
+
 function App() {
   return (
     <div>
       <CountProvider>
+        {/* move any of these out of CountProvider and open console to see error message */}
         <CountDisplay />
         <Counter />
       </CountProvider>
